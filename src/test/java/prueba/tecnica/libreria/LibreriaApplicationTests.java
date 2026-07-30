1bf9394926c1e1fc152b46059f26ee7d48c905b2
Julian Lopez <julian.lopez-b@mail.escuelaing.edu.co>
2026-07-30 17:01:36 -0500

    add: inicializing initial spring configuration

---
+++ src/test/java/prueba/tecnica/libreria/LibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.tecnica.libreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
